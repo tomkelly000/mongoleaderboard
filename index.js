0218f834044aed8c5764261947e1ec46881f7d90
Tom Kelly <tomkelly000@gmail.com>
2013-07-01 18:30:32 -0400

    cloned defaultoptions

--- index.js
+++ index.js
@@ -23,8 +23,16 @@ var default_options = {
 // constructor for making a new leaderboard
 function LeaderBoard(dburi, _options) {
 
-    // set the options
-    this.options = default_options;
+    // clone the object
+    this.options.collection = default_options.collection;
+    this.options.page_size = default_options.page_size;
+    this.options.num_pages = default_options.num_pages;
+    this.start_time.milliseconds = default_options.start_time.milliseconds;
+    this.start_time.seconds = default_options.start_time.seconds;
+    this.start_time.minutes = default_options.start_time.minutes;
+    this.start_time.hours = default_options.start_time.hours;
+    this.start_time.duration = default_options.duration;
+
     if (_options) {
 	if (_options.collection) 
 	    this.options.collection = [_options.collection];
@@ -74,143 +82,153 @@ function LeaderBoard(dburi, _options) {
     this.highscores = this.db.collection(this.options.collection[0]);
     this.refresh = this.db.collection(this.options.collection[1]);
 
-    var lb = this;
-
-    lb.refresh.find(null, function(er, time) {
+    var self = this;
+    this.refresh.find(null, function(er, time) {
 	    console.log('\n\n\n\n\n\n\n\n\n\n\n');
-    console.log(lb.options);
-    console.log('\n\n\n\n\n\n\n\n\n\n\n');
+	    console.log(self.options);
+	    console.log('\n\n\n\n\n\n\n\n\n\n\n');
 	    if (!time || time.length == 0) {
 		// first time launch
 		// calculate time for next refresh
 		var firstRefresh = new Date(); // starting point
-		console.log(lb.options);
-		firstRefresh.setMilliseconds(lb.options.start_time.milliseconds);
+		console.log(self.options);
+		firstRefresh.setMilliseconds(
+		    self.options.start_time.milliseconds);
 		firstRefresh.setSeconds(
-		    lb.options.start_time.seconds);
+		    self.options.start_time.seconds);
 		firstRefresh.setMinutes(
-                    lb.options.start_time.minutes);
+                    self.options.start_time.minutes);
 		firstRefresh.setHours(
-		    lb.options.start_time.hours);
+		    self.options.start_time.hours);
 		var nextRefresh;
-		if (lb.options.duration === Number.POSITIVE_INFINITY) {
+		if (self.options.duration === Number.POSITIVE_INFINITY) {
 		    nextRefresh = Number.POSITIVE_INFINITY;
 		} else {
 		    var nextRefresh = firstRefresh.getTime() +
 			Math.ceil(((new Date()).getTime() - firstRefresh.getTime())
-				  / lb.options.duration) * lb.options.duration;
+			      / self.options.duration) * self.options.duration;
 		}
-		lb.refresh.save({'nextRefresh':nextRefresh});	
+		self.refresh.save({'nextRefresh':nextRefresh});	
 	    }
 	});
+    this.limit = this.options.page_size * this.options.num_pages;
+}
 
-    // functions
-    this.test = function() {
-	return { 'options':lb.options, 'db':lb.db, 
-		 'collection':lb.highscores, 'refresh':lb.refresh };
-    }
+// instance methods
+LeaderBoard.prototype.test = function() {
+    return { 'options':this.options, 'db':this.db, 
+	     'collection':this.highscores, 'refresh':this.refresh };
+}
 
-
-    var limit = lb.options.page_size * lb.options.num_pages;
-
-    this.refresher = function(callback) {
-	var curTime = (new Date()).getTime();
-	lb.refresh.find({}, function(err, date) {
-		date = date[0];
-	if (curTime > date.nextRefresh) {
-	    var nextRefresh = date.nextRefresh +
-		Math.ceil((curTime - date.nextRefresh) / lb.options.duration)
-		* lb.options.duration;
-	    // update next refresh in database
-	    lb.refresh.update({}, {$set:{'nextRefresh':nextRefresh}});
-	    // it's time to refresh
-	    lb.highscores.remove(callback);
-	} else {
-	    callback();
-	}
+LeaderBoard.prototype.refresher = function(callback) {
+    var refresh = this.refresh;
+    var highscores = this.highscores;
+    var options = this.options;
+    var curTime = (new Date()).getTime();
+    this.refresh.find({}, function(err, date) {
+	    date = date[0];
+	    if (curTime > date.nextRefresh) {
+		var nextRefresh = date.nextRefresh +
+		    Math.ceil((curTime - date.nextRefresh) / options.duration)
+		    * options.duration;
+		// update next refresh in database
+		refresh.update({}, {$set:{'nextRefresh':nextRefresh}});
+		// it's time to refresh
+		highscores.remove(callback);
+	    } else {
+		callback();
+	    }
 	});
-    }
-
-    // executes the callback passing the argument true (1) 
-    // if the score is a high score, otherwise
-    // pass  false (0)
-    this.check = function(score, callback) {
-      lb.refresher(function() {
-	lb.highscores.find().sort({score:1}, function(err, scores) {
-		if (!scores || scores.length < limit) {
-		    // we'll take any scores at this point!
-		    callback(1);
-		} else {
-		    if (score > scores[0].score) {
-			// larger than lowest score on leaderboard
-			callback(1);
-		    } else 
-			callback(0);
-		} 
-	    });
-	  });
-    }
-
-    // overwrites the lowest score on the leaderboard if the given score 
-    // is larger. if the force parameter is supplied, it will overwrite
-    // no matter what
-    this.postHighScore = function(highscore, member, callback, force) {
-	if (arguments.length <= 3) force = false;
-	member.score = highscore; // set the score 
-      lb.refresher(function() {
-	lb.highscores.find().sort({score:1}, function(err, scores) {
-		if (!scores || scores.length < limit) {
-		    // leaderboard isn't full yet go ahead and save
-		    lb.highscores.save(member, {safe:true},
-				    function(err, score)
-				    {callback(err, score)});
-		} else {
-		    // overwrite lowest score unless force is supplied
-		    if (highscore > scores[0].score || force) {
-			lb.highscores.update({_id:scores[0]._id},
-					  member, {safe:true},
-					  function(err, score)
-					  {callback(err, score)});
-		    } else {
-			callback(null, member);
-		    }
-		}
-	    });
-	  });
-    }
-
-    // an alias
-    this.post = this.postHighScore;
-
-    // executes the callback on the two arguments err and scores,
-    // where scores is an array of objects
-    this.getHighScores = function(callback) {
-      lb.refresher(function() {
-	lb.highscores.find().sort({score:-1}, function(err, scores)
-                                            {callback(err, scores)});
-	  });
-    }
+}
     
-    // an alias
-    this.get = this.getHighScores;
+// executes the callback passing the argument true (1) 
+// if the score is a high score, otherwise
+// pass  false (0)
+LeaderBoard.prototype.check = function(score, callback) {
+    var highscores = this.highscores;
+    var limit = this.limit;
+    this.refresher(function() {
+	    highscores.find().sort({score:1}, function(err, scores) {
+		    if (!scores || scores.length < limit) {
+			// we'll take any scores at this point!
+			callback(1);
+		    } else {
+			if (score > scores[0].score) {
+			    // larger than lowest score on leaderboard
+			    callback(1);
+			} else 
+			    callback(0);
+		    } 
+		});
+	});
+}
+    
+// overwrites the lowest score on the leaderboard if the given score 
+// is larger. if the force parameter is supplied, it will overwrite
+// no matter what
+LeaderBoard.prototype.postHighScore = function(highscore, member, callback, force) {
+    if (arguments.length <= 3) force = false;
+    member.score = highscore; // set the score 
+    
+    var highscores = this.highscores;
+    var limit = this.limit;
+    this.refresher(function() {
+	    highscores.find().sort({score:1}, function(err, scores) {
+		    if (!scores || scores.length < limit) {
+			// leaderboard isn't full yet go ahead and save
+			highscores.save(member, {safe:true},
+					function(err, score)
+					{callback(err, score)});
+		    } else {
+			// overwrite lowest score unless force is supplied
+			if (highscore > scores[0].score || force) {
+			    highscores.update({_id:scores[0]._id},
+					      member, {safe:true},
+					      function(err, score)
+					      {callback(err, score)});
+			} else {
+			    callback(null, member);
+			}
+		    }
+		});
+	});
+}
+    
+// an alias
+LeaderBoard.prototype.post = LeaderBoard.prototype.postHighScore;
 
-    // execute the callback on only the highscores on page number page_num
-    this.getPage = function(page_num, callback) {
-	page_num = page_num - 1; // pages start at 1 instead of 0
-	assert(page_num >= 0 && page_num < options.num_pages, 
-	       'Invalid page number');
-      lb.refresher(function() {
-	lb.highscores.find().sort({score:-1}, function(err, scores) {
-		if (scores) {
-		    var index = page_num * options.page_size;
-		    var page = scores.splice(index, options.page_size);
-		    callback(err, page);
-		} else {
-		    callback(null, []);
-		}
-	    });
-	  });
-    }
+// executes the callback on the two arguments err and scores,
+// where scores is an array of objects
+LeaderBoard.prototype.getHighScores = function(callback) {
+    var highscores = this.highscores;
+    this.refresher(function() {
+	    highscores.find().sort({score:-1}, function(err, scores)
+				   {callback(err, scores)});
+	});
+}
+    
+// an alias
+LeaderBoard.prototype.get = LeaderBoard.prototype.getHighScores;
+
+// execute the callback on only the highscores on page number page_num
+LeaderBoard.prototype.getPage = function(page_num, callback) {
+    page_num = page_num - 1; // pages start at 1 instead of 0
+    assert(page_num >= 0 && page_num < options.num_pages, 
+	   'Invalid page number');
+    
+    var highscores = this.highscores;
+    var options = this.options;
+    this.refresher(function() {
+	    highscores.find().sort({score:-1}, function(err, scores) {
+		    if (scores) {
+			var index = page_num * options.page_size;
+			var page = scores.splice(index, options.page_size);
+			callback(err, page);
+		    } else {
+			callback(null, []);
+		    }
+		});
+	});
 }
 
 module.exports = LeaderBoard; // a constructor 
